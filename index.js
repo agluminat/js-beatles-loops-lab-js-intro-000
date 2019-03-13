a12764ccde48c1021b11a05ffc252902f2df2072
Maxi Learner <sagmeister@gmail.com>
2019-03-13 23:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function johnLennonFacts(facts) {
   }
   return facts
 }
+
+function iLoveTheBeatles(num) {
+  var emptArray = []
+  do {
+    emptArray.push("I love the Beatles!")
+    num++
+  } while num < 15
+  return emptArray
+}
